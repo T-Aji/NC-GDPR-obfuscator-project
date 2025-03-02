3d1b366c6b54ec3e813e957371d37ba919fbd05f
T-Aji <166807537+T-Aji@users.noreply.github.com>
2025-03-02 13:52:51 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     name="obfuscator",
     version="0.1.0",
     author="Tolu Ajibade",
     description="A library to obfuscate PII in files stored in S3.",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=find_packages(where="src"),
+    packages=find_packages(where="src", include=["src", "src.*", "tests", "tests.*"]),
     package_dir={"": "src"},
     install_requires=requirements,
     python_requires=">=3.7",
@@ -21,7 +24,11 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "obfuscator=src.main:obfuscator",  # CLI entry point
+            "obfuscator=src.main:obfuscator",
         ],
     },
+    package_data={
+        "obfuscator": ["data/*.json", "config/*.ini"],
+    },
+    include_package_data=True,
 )
